b2b3d94932745c7f14e4c0de644b3d5f9b152e0e
Kumar_Sydygaliev <sk12376@auca.kg>
2025-01-21 12:01:47 +0600

    lab7 completed

---
+++ lab7/exercise1.py
@@ -0,0 +1,16 @@
+#1 Printing numbers from 1 to 10
+print("Using \"for\" loop:")
+for i in range(1, 11):
+    print(i)
+
+print("\nUsing \"while\" loop:")
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+print("")
+#2 Iterating through a string
+string = "Python"
+for char in string:
+    print(char)
