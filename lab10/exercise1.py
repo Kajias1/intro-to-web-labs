0bdeccc3e82b5781ba9c2f5abef0c233392458b1
Kumar_Sydygaliev <sk12376@auca.kg>
2025-01-28 11:35:25 +0600

    lab10 completed

---
+++ lab10/exercise1.py
@@ -0,0 +1,13 @@
+import mymodule as mx
+import platform
+
+mx.greeting("Jonathan")
+
+a = mx.person1["age"]
+print(a)
+
+x = platform.system()
+print(x)
+
+x = dir(platform)
+print(x)
---
+++ lab10/exercise3.py
@@ -0,0 +1,34 @@
+import math
+
+# Built-in "min" and "max" functions
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+# Built-in "abs" function
+x = abs(-7.25)
+
+print(x)
+
+# Built-in "pow" function
+x = pow(4, 3)
+
+print(x)
+
+# Using "math" module
+x = math.sqrt(64)
+
+print(x)
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+# PI constant
+x = math.pi
+
+print(x)
