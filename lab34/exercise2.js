ec480f3122a3e43b1cda97fb9aba88d3e6e097cd
BuildTools <unconfigured@null.spigotmc.org>
2025-03-11 10:19:46 +0600

    lab34 completed

---
+++ lab34/exercise2.js
@@ -0,0 +1,12 @@
+let age = 20;
+let isRegistered = true;
+
+if (age >= 18) {
+    if (isRegistered) {
+        console.log("You are eligible to vote.");
+    } else {
+        console.log("You need to register to vote");
+    }
+} else {
+    console.log("You are not eligible to vote.");
+}
---
+++ lab34/exercise5.js
@@ -0,0 +1,16 @@
+let day = "Tuesday";
+
+switch(day) {
+    case "Monday":
+        console.log("Start of the week!");
+        break;
+    case "Friday":
+        console.log("Tomorrow is the weekend!");
+        break;
+    case "Sunday":
+        console.log("It's a rest day.");
+        break;
+    default:
+        console.log("It's a regular day.");
+        break;
+}
